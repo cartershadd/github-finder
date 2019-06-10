93c794727d2211b866674285a058c1f7f0407650
Carter Shadden <carter.shadden@gmail.com>
2019-06-10 12:39:51 -0400

    Pushing project to github

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React, { Fragment, Component } from 'react';
+import spinner from './spinner.gif';
+
+class Spinner extends Component {
+    render() {
+        return (
+            <div>
+                <img src={'./spinner.gif)'} alt="Loading..." className='spinner'
+                     style={{ width: '200px', margin: 'auto', display: 'block'}} />
+            </div>
+        );
+    }
+}
+
+export default Spinner;
